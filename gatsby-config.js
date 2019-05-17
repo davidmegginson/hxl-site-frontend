3d4135bec92146f05de1a0f0130f058d422c5369
Serban Teodorescu <teodorescu.serban@gmail.com>
2019-05-17 15:23:24 +0300

    point gatsby to the dev blog

--- gatsby-config.js
+++ gatsby-config.js
@@ -47,7 +47,7 @@ module.exports = {
         * The base URL of the WordPress site without the trailingslash and the protocol. This is required.
         * Example : 'gatsbyjswpexample.wordpress.com' or 'www.example-site.com'
         */
-        baseUrl: `hxl2.local`,
+        baseUrl: `blog.dev.hxlstandard.org`,
         // The protocol. This can be http or https.
         protocol: `http`,
         // Indicates whether the site is hosted on wordpress.com.
